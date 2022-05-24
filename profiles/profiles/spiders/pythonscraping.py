09b83d05dedc7b133bd0a9b37bcd9a7bfd90b0fc
sahuankit010 <32036503+sahuankit010@users.noreply.github.com>
2022-05-23 21:57:48 -0500

    final code for profiles

--- profiles/profiles/spiders/pythonscraping.py
+++ profiles/profiles/spiders/pythonscraping.py
@@ -4,7 +4,14 @@ import scrapy
 class PythonscrapingSpider(scrapy.Spider):
     name = 'pythonscraping'
     allowed_domains = ['pythonscraping.com']
-    start_urls = ['http://pythonscraping.com/']
+    start_urls = ['http://pythonscraping.com/linkedin/cookies/profile.php']
 
+    def make_requests_from_url(self, url):
+        request = super(PythonscrapingSpider, self).make_requests_from_url(url)
+        request.cookies['username'] = 'ankit!!!'
+        request.cookies['loggedin'] = 1
+        return request
     def parse(self, response):
-        pass
+        return {
+            'text': response.xpath('//body/text()').get()
+        }
