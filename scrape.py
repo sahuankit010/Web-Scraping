ac3dfa9ddc9095ee95aa2ab88114c4488dfee933
sahuankit010 <32036503+sahuankit010@users.noreply.github.com>
2022-05-24 21:28:07 -0500

    updating findall of scrape.py

--- scrape.py
+++ scrape.py
@@ -11,3 +11,39 @@ with open('simple.html') as html_file:
 print(soup)
 
 print(soup.prettify())
+
+#getting the title of the simple.html
+matchTitle = soup.title
+matchTitleText = soup.title.text
+
+print(matchTitle)
+print(matchTitleText)
+
+#getting the div
+
+matchDiv = soup.div
+print(matchDiv)
+
+#getting tag using find method
+
+matchUsingFind = soup.find('div', class_ = 'footer')
+
+print(matchUsingFind)
+
+article = soup.find('div', class_ = 'article')
+
+headline = article.h2.a.text
+
+print(headline)
+
+summary = article.p.text
+
+print(summary)
+
+#find_all method
+
+for article in soup.find_all('div', class_= 'article'):
+    headline = article.h2.a.text
+    print(headline)
+    summary = article.p.text
+    print(summary)
