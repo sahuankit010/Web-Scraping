80d4f8cc13bd177ea91103fd3c9bf25c444155f4
sahuankit010 <32036503+sahuankit010@users.noreply.github.com>
2022-05-25 20:29:24 -0500

    finish this script

--- hosted-web-page-scrape.py
+++ hosted-web-page-scrape.py
@@ -3,17 +3,21 @@
 
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 source =  requests.get('https://coreyms.com').text
 # print(source)
 soup = BeautifulSoup(source, 'lxml')
-
+#writing to a file here
+csv_file = open('cms.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary', 'video_link'])
 # print(soup.prettify())
 for article in soup.find_all('article'):
     headline = article.h2.a.text
     # print(headline)
 
-    # summary = article.find('div', class_ = 'entry-content').p.text
+    summary = article.find('div', class_ = 'entry-content').p.text
 
     # print(summary)
     if article is not None:
@@ -25,3 +29,7 @@ for article in soup.find_all('article'):
     if ytID is not None:
         yt_link = f'https://youtube.com/watch?v={ytID}'
         print(headline + " " + yt_link)
+        csv_writer.writerow([headline,summary,yt_link])
+
+
+csv_file.close()        
