d26567cab8e0dce20e44bd6b24fbfb6b363b4f2f
sahuankit010 <32036503+sahuankit010@users.noreply.github.com>
2022-05-25 20:19:25 -0500

    all links in a page

--- hosted-web-page-scrape.py
+++ hosted-web-page-scrape.py
@@ -9,21 +9,19 @@ source =  requests.get('https://coreyms.com').text
 soup = BeautifulSoup(source, 'lxml')
 
 # print(soup.prettify())
+for article in soup.find_all('article'):
+    headline = article.h2.a.text
+    # print(headline)
 
-article = soup.find('article')
+    # summary = article.find('div', class_ = 'entry-content').p.text
 
-headline = article.h2.a.text
-# print(headline)
-
-summary = article.find('div', class_ = 'entry-content').p.text
-
-# print(summary)
-
-ytspan = article.find('span', class_='embed-youtube')
+    # print(summary)
+    if article is not None:
+        ytspan = article.find('span', class_='embed-youtube')
 
 # print(ytspan.prettify())
-
-ytID = ytspan.find('iframe', class_='youtube-player')['src'].split('/')[4].split('?')[0]
-
-yt_link = f'https://youtube.com/watch?v={ytID}'
-print(yt_link)
+    if ytspan is not None:
+        ytID = ytspan.find('iframe', class_='youtube-player')['src'].split('/')[4].split('?')[0]
+    if ytID is not None:
+        yt_link = f'https://youtube.com/watch?v={ytID}'
+        print(headline + " " + yt_link)
